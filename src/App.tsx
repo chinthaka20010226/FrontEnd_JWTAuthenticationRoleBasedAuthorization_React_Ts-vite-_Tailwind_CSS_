1a02f3c5ac8a5705915b737cf07bcc2e66c730a7
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-12 18:07:56 +0530

    Toaster is -> Next page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from "react-hot-toast"
 import GlobalRouter from "./routes"
 
 const App = () => {
   return (
     <div>
       <GlobalRouter />
+      {/* Toaster is -> Next page */}
+      <Toaster />
     </div>
   )
 }
